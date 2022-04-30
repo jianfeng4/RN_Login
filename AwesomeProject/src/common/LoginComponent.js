a1627093643785ba56f066287a5ac1fe3248ce70
shengjianfeng <jianfeng2612282@163.com>
2022-04-30 11:36:42 +0800

    第一次提交_初始化项目

---
+++ AwesomeProject/src/common/LoginComponent.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function LoginComponent() {
+  return (
+    <View>
+      <Text>LoginComponent</Text>
+    </View>
+  )
+}
+
+export default function LoginComponent() {
+    return (
+      <View>
+        <Text>LoginComponent</Text>
+      </View>
+    )
+  }
+  
+  export default function LoginComponent() {
+    return (
+      <View>
+        <Text>LoginComponent</Text>
+      </View>
+    )
+  }
+
+const styles = StyleSheet.create({})
